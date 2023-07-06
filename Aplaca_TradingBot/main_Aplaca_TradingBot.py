8f6076483751047ef642497a5f9155e1ce46819b
Andi <120022290+Andi-it-is@users.noreply.github.com>
2023-07-06 13:30:49 +0200

    Add files via upload

--- Aplaca_TradingBot/main_Aplaca_TradingBot.py
+++ Aplaca_TradingBot/main_Aplaca_TradingBot.py
@@ -8,7 +8,7 @@ from alpaca.data.requests import*
 import yfinance as yf
 import pandas as pd
 import json
-
+from alpaca.data.live import*
 
 trading_client = TradingClient(config['API_KEY'], config['SECRET_KEY'], paper=True)
 api = tradeapi.REST(config['API_KEY'], config['SECRET_KEY'], config['paper_url'])
@@ -29,9 +29,9 @@ market_order_data = MarketOrderRequest(
 
 
 #Submitting the order and then printing the returned object
-market_order = trading_client.submit_order(market_order_data)
-for property_name, value in market_order:
-  print(f"\"{property_name}\": {value}")
+# market_order = trading_client.submit_order(market_order_data)
+# for property_name, value in market_order:
+#   print(f"\"{property_name}\": {value}")
 
 
 
@@ -43,9 +43,9 @@ market_order_data = MarketOrderRequest(
                       time_in_force=TimeInForce.DAY                     
                   )
 
-market_order = trading_client.submit_order(market_order_data)
-for property_name, value in market_order:
-  print(f"\"{property_name}\": {value}")
+# market_order = trading_client.submit_order(market_order_data)
+# for property_name, value in market_order:
+#   print(f"\"{property_name}\": {value}")
 
 
 
@@ -68,3 +68,40 @@ moving_averages = data[['200DMA']].to_dict(orient='index')
 # Save the moving averages as a JSON file
 with open('moving_averages.json', 'w') as file:
     file.write(json.dumps(moving_averages, indent=4, sort_keys=True, default=str))
+print(data)
+
+
+
+
+#Eigene Implentierung von 50 DMA
+data = yf.download(ticker, period= "3mo")  # Letzten 3 Monate anschauen
+
+data_50DMA = data['Close'].rolling(window=50).mean()  # Arithmetisches Mittel von den letzten 50 close daten bilden
+
+data_50DMA.index = data_50DMA.index.strftime('%Y-%m-%d') #ins Datetime format bringen
+
+moving_averages_50DMA = data_50DMA.to_dict() # in Dict umwandeln
+
+with open('moving_averages_50DMA.json', 'w') as file:                                               #json erstellen
+    file.write(json.dumps(moving_averages_50DMA, indent=4, sort_keys=True, default=str))
+
+
+
+
+
+
+# print(data['50DMA'])
+
+
+
+# stock_stream = StockDataStream(config['API_KEY'], config['SECRET_KEY'])
+
+# async def bar_callback(bar):
+#     for property_name, value in bar:
+#         print(f"\"{property_name}\": {value}")
+
+# # Subscribing to bar event 
+# symbol = "AAPL"
+# stock_stream.subscribe_bars(bar_callback, symbol)
+
+# stock_stream.run()
