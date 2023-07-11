3106452a5e86014e5fa50d4a3b86aba250110f9f
Andi <120022290+Andi-it-is@users.noreply.github.com>
2023-07-11 12:50:55 +0200

    Update 11.07
    Neuester Stand, mit 2 jsons, der Main Datei und 2 side Dateien (config die nicht gepusht wird, da drin ist paper url, api key und api secret) sowie einer Datei "Orders_erstellen" in der Zukünftig jede Instanziierung passieren soll. Aktuell sind es nur Variablen mit unterschiedlichen Order typen drin. 
    Später kann so ein dataframe wie aktuell in der main am Beispiel apple auch dort rein. Dann ist die Main weniger visual clutter

--- Aplaca_TradingBot/main_Aplaca_TradingBot.py
+++ Aplaca_TradingBot/main_Aplaca_TradingBot.py
@@ -1,5 +1,6 @@
 # Importing the API and instantiating the REST client according to our keys
-from localconfig import config #config Datei im Projekt  mit api key, secret key, paper url  (wird nicht mit gepusht)
+from localconfig import config    #config Datei im Projekt  mit api key, secret key, paper url  (wird nicht mit gepusht)
+from Orders_erstellen import*   #andere Datei wo Order instanzen erstellt werden. diese gilt es dann auszuführen
 import alpaca_trade_api as tradeapi
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import* # MarketOrderRequest, OrderRequest
@@ -8,72 +9,50 @@ from alpaca.data.requests import*
 import yfinance as yf
 import pandas as pd
 import json
+import numpy as np
 from alpaca.data.live import*
+
+
 
 trading_client = TradingClient(config['API_KEY'], config['SECRET_KEY'], paper=True)
 api = tradeapi.REST(config['API_KEY'], config['SECRET_KEY'], config['paper_url'])
 
+
+
 # Getting account information and printing it
-# account = trading_client.get_account()
-# for property_name, value in account:
-#   print(f"\"{property_name}\": {value}")
+account = trading_client.get_account()
+for property_name, value in account:
+  #print(f"\"{property_name}\": {value}")
 
 
-# Setting parameters for Buy order
-market_order_data = MarketOrderRequest(
-                      symbol="NVDA",
-                      qty=13,
-                      side=OrderSide.BUY,
-                      time_in_force=TimeInForce.DAY                     
-                  )
 
+# #Submitting order 
+# #market_order = trading_client.submit_order(order_google_buy)
 
-#Submitting the order and then printing the returned object
-# market_order = trading_client.submit_order(market_order_data)
+# # and then printing the returned object
 # for property_name, value in market_order:
 #   print(f"\"{property_name}\": {value}")
 
 
 
-# Setting parameters for Sell order
-market_order_data = MarketOrderRequest(
-                      symbol="MSFT",
-                      qty=30,
-                      side=OrderSide.SELL,
-                      time_in_force=TimeInForce.DAY                     
-                  )
+#Implementierung vom 200DMA von apple
+    ticker = 'AAPl'
 
-# market_order = trading_client.submit_order(market_order_data)
-# for property_name, value in market_order:
-#   print(f"\"{property_name}\": {value}")
+data = yf.download(ticker, period= "1y")  # Letzten 12 Monate anschauen
 
+data_200DMA = data['Close'].rolling(window=200).mean()  # Arithmetisches Mittel von den letzten 50 close daten bilden
 
+data_200DMA.index = data_200DMA.index.strftime('%Y-%m-%d') #ins Datetime format bringen
 
+moving_averages_200DMA = data_200DMA.to_dict() # in Dict umwandeln
 
-
-
-#######################################
-
-
-ticker = "AAPL"  # Ticker symbol for Apple
-data = yf.download(ticker, period="1y")  # Retrieve 1 year of data
-data['200DMA'] = data['Close'].rolling(window=200).mean()
-
-# Convert the timestamps to string format
-data.index = data.index.strftime('%Y-%m-%d')
-
-# Select only the '200DMA' column and convert it to a dictionary
-moving_averages = data[['200DMA']].to_dict(orient='index')
-
-# Save the moving averages as a JSON file
-with open('moving_averages.json', 'w') as file:
-    file.write(json.dumps(moving_averages, indent=4, sort_keys=True, default=str))
-print(data)
+with open('moving_averages_200DMA.json', 'w') as file:                                               #json erstellen
+    file.write(json.dumps(moving_averages_200DMA, indent=4, sort_keys=True, default=str))
 
 
 
 
-#Eigene Implentierung von 50 DMA
+#Implentierung von 50 DMA von apple
 data = yf.download(ticker, period= "3mo")  # Letzten 3 Monate anschauen
 
 data_50DMA = data['Close'].rolling(window=50).mean()  # Arithmetisches Mittel von den letzten 50 close daten bilden
@@ -87,21 +66,55 @@ with open('moving_averages_50DMA.json', 'w') as file:                           
 
 
 
+common_dates = set(moving_averages_50DMA.keys()).intersection(set(moving_averages_200DMA.keys()))
+
+# Extract the values for the common dates, excluding NaN values
+value50DMA = [moving_averages_50DMA[date] for date in common_dates if not np.isnan(moving_averages_50DMA[date])]
+value200DMA = [moving_averages_200DMA[date] for date in common_dates if not np.isnan(moving_averages_200DMA[date])]
+value200DMA = value200DMA[-len(value50DMA):]
+
+print(value50DMA)
+print(len(value50DMA))
+
+print(value200DMA)
+print(len(value200DMA))
 
 
 
-# print(data['50DMA'])
+
+df = pd.DataFrame({'50DMA': value50DMA, '200DMA': value200DMA})
+
+print(df)
+
+
+# Initialize trading parameters
+short_period = 50
+long_period = 200
+current_status = "neutral"
+trading_signals = []
+
+# Iterate through the historical data
+for i in range(0, len(df)):
+    if df['50DMA'].iloc[i] > df['200DMA'].iloc[i] and current_status == "neutral":
+        trading_signals.append("Buy")
+        current_status = "long"
+    elif df['50DMA'].iloc[i] < df['200DMA'].iloc[i] and current_status == "long":
+        trading_signals.append("Sell")
+        current_status = "neutral"
+    elif df['50DMA'].iloc[i] < df['200DMA'].iloc[i] and current_status == "neutral":
+        trading_signals.append("Sell")
+        current_status = "short"
+    elif df['50DMA'].iloc[i] > df['200DMA'].iloc[i] and current_status == "short":
+        trading_signals.append("Buy")
+        current_status = "neutral"
+    else:
+        trading_signals.append("Hold")
+
+# Add the trading signals to the DataFrame
+df['Signal'] = trading_signals
+
+print(df)
 
 
 
-# stock_stream = StockDataStream(config['API_KEY'], config['SECRET_KEY'])
 
-# async def bar_callback(bar):
-#     for property_name, value in bar:
-#         print(f"\"{property_name}\": {value}")
-
-# # Subscribing to bar event 
-# symbol = "AAPL"
-# stock_stream.subscribe_bars(bar_callback, symbol)
-
-# stock_stream.run()
