d790dd214780a4670365018d3e0bd83382825f5b
Andi <120022290+Andi-it-is@users.noreply.github.com>
2023-07-14 14:31:16 +0200

    Final version

---
+++ final_Aplaca_TradingBot/Daten.py
@@ -0,0 +1,232 @@
+from alpaca.trading.requests import* # MarketOrderRequest, OrderRequest
+from alpaca.trading.enums import* # OrderSide, TimeInForce
+import json
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+#Alle 6 BUY Orders als Variable für unsere betrachteten Aktien, standardisiert auf .DAY Order und 10 Stück
+
+order_apple_buy = MarketOrderRequest(
+                      symbol="AAPL",
+                      qty=10,
+                      side=OrderSide.BUY,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_tesla_buy = MarketOrderRequest(
+                      symbol="TSLA",
+                      qty=10,
+                      side=OrderSide.BUY,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_microsoft_buy = MarketOrderRequest(
+                      symbol="MSFT",
+                      qty=10,
+                      side=OrderSide.BUY,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_nvidia_buy = MarketOrderRequest(
+                      symbol="NVDA",
+                      qty=10,
+                      side=OrderSide.BUY,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_meta_buy = MarketOrderRequest(
+                      symbol="META",
+                      qty=10,
+                      side=OrderSide.BUY,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_google_buy = MarketOrderRequest(
+                      symbol="GOOGL",
+                      qty=10,
+                      side=OrderSide.BUY,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+
+#Alle 6 SELL Orders als Variable für unsere betrachteten Aktien, standardisiert auf .DAY Order und 10 Stück
+
+order_apple_sell = MarketOrderRequest(
+                      symbol="AAPL",
+                      qty=10,
+                      side=OrderSide.SELL,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_tesla_sell = MarketOrderRequest(
+                      symbol="TSLA",
+                      qty=10,
+                      side=OrderSide.SELL,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_microsoft_sell = MarketOrderRequest(
+                      symbol="MSFT",
+                      qty=10,
+                      side=OrderSide.SELL,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_nvidia_sell = MarketOrderRequest(
+                      symbol="NVDA",
+                      qty=10,
+                      side=OrderSide.SELL,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_meta_sell = MarketOrderRequest(
+                      symbol="META",
+                      qty=10,
+                      side=OrderSide.SELL,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+order_google_sell = MarketOrderRequest(
+                      symbol="GOOGL",
+                      qty=10,
+                      side=OrderSide.SELL,
+                      time_in_force=TimeInForce.DAY                     
+                  )
+
+
+
+#Funktionen
+
+
+def json_create(ticker: str, period: str):
+
+    """_summary_
+
+    Args:
+        ticker (str): Aktien Kürzel; erlaubte Werte sind      AAPL, TSLA, MSFT, NVDA, META, GOOGL
+        period (str): Zeiträume die Werte für Aktien beinhaltet; "1y" für 200DMA , "3mo" für 50DMA
+
+    Returns:
+        json: Entsprechende erstellte json der jeweiligen Aktie mit dem jeweiligen ticker e.g. GOOGL_50DMA.json
+    """
+
+
+    if period == "1y":
+        stock_daten = yf.download(ticker, period="1y")
+        stock_DMA_daten = stock_daten['Close'].rolling(window=200).mean()
+
+    elif period == "3mo":
+        stock_daten = yf.download(ticker, period="3mo")
+        stock_DMA_daten = stock_daten['Close'].rolling(window=50).mean()
+    else:
+        Exception
+
+    stock_DMA_daten.index = stock_DMA_daten.index.strftime('%Y-%m-%d')
+    DMA = stock_DMA_daten.to_dict()
+    
+    if period == "1y":
+        with open(ticker + '_200DMA.json', 'w') as file:                                               #json erstellen
+            file.write(json.dumps(DMA, indent=4, sort_keys=True, default=str))
+    else:
+        with open(ticker + '_50DMA.json', 'w') as file:                                               #json erstellen
+            file.write(json.dumps(DMA, indent=4, sort_keys=True, default=str))
+
+
+
+    return DMA
+
+
+def df_create(ticker: str , json50DMA : json, json200DMA : json):
+
+    """die 2 übergebenen jsons sollten von der selben Aktie sein
+    technisch ist es möglich ein df von e.g. apple_50DMA und nvidia_200DMA zu erstellen, jedoch ist das Nonsense.
+    Lässt man alles über check_signals() erstellen, dann geht das jedoch nicht.
+
+    Args:
+        json50DMA (json): json50DMA deiner Aktie,   vorher mit json_create() erstellen
+        json200DMA (json): json200DMA deiner Aktie  vorher mit json_create() erstellen
+
+    Returns:
+        df (dataframe): erstelltes Dataframe mit 3 Spalten; gemeinsames Datum, Werte für 50DMA, Werte für 200DMA 
+    """
+
+    #NaN rausfiltern
+    json50DMA = {date: value for date, value in json50DMA.items() if not pd.isna(value)}
+    json200DMA = {date: value for date, value in json200DMA.items() if not pd.isna(value)}
+
+    common_dates = sorted(set(json50DMA.keys()).intersection(set(json200DMA.keys())))
+
+    new_df = pd.DataFrame({
+        'stock_name' : ticker,
+        'common_dates': common_dates,
+        '50DMA': [json50DMA[date] for date in common_dates],
+        '200DMA': [json200DMA[date] for date in common_dates]
+})
+
+    return new_df
+
+
+def df_update(df):                              #Argument ist das entsprechende df was geupdatet werden soll
+    """_summary_
+
+    Args:
+        df (dataframe): Input dataframe welches um den Reiter "Signal" erweitert werden soll. In der Funktion check_stock() bzw run_script() wird dieses df erstellt
+
+    Returns:
+        df (dataframe): erweitertes dataframe mit den Reitern Signal und dessen Values Buy,Hold,Sell  
+    """
+
+    # Initialize trading parameters
+    short_period = 50
+    long_period = 200
+    current_status = "neutral"
+    trading_signals = []
+
+
+    # Iterate through the historical data
+    for i in range(0, len(df)):
+        if df['50DMA'].iloc[i] > df['200DMA'].iloc[i] and current_status == "neutral":
+            trading_signals.append("Buy")
+            current_status = "long"
+        elif df['50DMA'].iloc[i] < df['200DMA'].iloc[i] and current_status == "long":
+            trading_signals.append("Sell")
+            current_status = "neutral"
+        elif df['50DMA'].iloc[i] < df['200DMA'].iloc[i] and current_status == "neutral":
+            trading_signals.append("Sell")
+            current_status = "short"
+        elif df['50DMA'].iloc[i] > df['200DMA'].iloc[i] and current_status == "short":
+            trading_signals.append("Buy")
+            current_status = "neutral"
+        else:
+            trading_signals.append("Hold")
+
+    # Add the trading signals to the DataFrame
+    df['Signal'] = trading_signals
+
+
+    print(df)
+    return df
+
+
+
+
+
+#Versuch von Custom Käufen außerhalb unserer 6 Standard methoden
+
+from typing import Literal
+
+class MarketOrderRequest:
+    def __init__(self, symbol: str, qty: int, side: Literal['buy', 'sell'], time_in_force: Literal['GTC', 'IOC', 'FOK']):
+        self.symbol = symbol
+        self.qty = qty
+        self.side = side
+        self.time_in_force = time_in_force
+
+def custom_order(symbol: str, qty: int, side: Literal['buy', 'sell'], time_in_force: Literal['GTC', 'IOC', 'FOK']):
+    order = MarketOrderRequest(
+        symbol=symbol,
+        qty=qty,
+        side=side,
+        time_in_force=time_in_force
+    )
---
+++ final_Aplaca_TradingBot/main_Aplaca_TradingBot.py
@@ -0,0 +1,139 @@
+#Imports
+from localconfig import config          #config Datei im Projekt  mit api key, secret key, paper url  (wird nicht mit gepusht)
+from Daten import*                      #Datei zum Ablagern von Funktionen
+import alpaca_trade_api as tradeapi
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import* 
+from alpaca.trading.enums import*
+from alpaca.data.requests import*
+import time
+
+#Api Instanz erstellen
+trading_client = TradingClient(config['API_KEY'], config['SECRET_KEY'], paper=True)
+api = tradeapi.REST(config['API_KEY'], config['SECRET_KEY'], config['paper_url'])
+
+#Aktuelle Accountinformationen einholen
+account = trading_client.get_account()
+for property_name, value in account:
+  print(f"\"{property_name}\": {value}")
+
+
+
+def check_stock(ticker : str):
+
+  """ Funktion, welche die oben genannte Aktie 'überprüft'
+  Zuerst werden Daten gesammelt, formatiert, ausgefiltert und zusammengefügt
+  Danach wird der Reiter 'Signal' erstellt, für jedes Datum wird eine Kaufentscheidung beurteilt und diese
+  wird hinzugefügt in das Dataframe
+   
+  Args:
+      ticker (str): Erlaubte Aktienkürzel: 
+  Returns:
+      Aussage (str): Information, dass Aktie analysiert wurde
+      df (Dataframe): siehe df_update()
+   """
+  
+    
+  # #df muss neu generiert + neu updatet werden bzw die jsons müssen alle 24h neu erstellt werden, um mit """aktuellen""" Daten zu traden
+  var1 = json_create(ticker,"3mo")
+  var2 = json_create(ticker,"1y")
+  old_df = df_create(ticker, var1, var2)
+  df = df_update(old_df)
+
+
+  # Kontrolliert ob 'Buy' in der Spalte Signal ist und ob es der letzte Eintrag(und somit der aktuellste) ist
+  if 'Buy' in df['Signal'] and 'Buy' in df.iloc[len(df)-1]:     #im Prinzip doppelt gemoppelt, denn wenn das 2. stimmt, stimmt auch das erste
+      
+      if df['stock_name'] == 'AAPL':
+        print('10x apple gekauft')
+        trading_client.submit_order(order_apple_buy)
+
+      elif df['stock_name'] == 'TSLA':
+        print('10x tesla gekauft')
+        trading_client.submit_order(order_tesla_buy)
+
+      elif df['stock_name'] == 'MSFT':
+        print('10x MSFT gekauft')
+        trading_client.submit_order(order_microsoft_buy) 
+
+      elif df['stock_name'] == 'NVDA':
+        print('10x NVDA gekauft')
+        trading_client.submit_order(order_nvidia_buy)
+
+      elif df['stock_name'] == 'META':
+        print('10x META gekauft')
+        trading_client.submit_order(order_meta_buy)
+
+      elif df['stock_name'] == 'GOOGL':
+        print('10x GOOGL gekauft')
+        trading_client.submit_order(order_google_buy)
+
+
+      else:
+        print("Order nicht möglich, verwende Custome_order()")
+
+
+
+  # Kontrolliert ob 'Sell' in der Spalte Signal ist und ob es der letzte Eintrag(und somit der aktuellste) ist
+  if 'Sell' in df['Signal'] and 'Sell' in df.iloc[len(df)-1]:
+      
+      if df['stock_name'] == 'AAPL':
+        print('10x apple verkauft')
+        trading_client.submit_order(order_apple_sell)
+
+      elif df['stock_name'] == 'TSLA':
+        print('10x tesla verkauft')
+        trading_client.submit_order(order_tesla_sell)
+
+      elif df['stock_name'] == 'MSFT':
+        print('10x MSFT verkauft')
+        trading_client.submit_order(order_microsoft_sell) 
+
+      elif df['stock_name'] == 'NVDA':
+        print('10x NVDA verkauft')
+        trading_client.submit_order(order_nvidia_sell)
+
+      elif df['stock_name'] == 'META':
+        print('10x META verkauft')
+        trading_client.submit_order(order_meta_sell)
+
+      elif df['stock_name'] == 'GOOGL':
+        print('10x GOOGL verkauft')
+        trading_client.submit_order(order_google_sell)
+
+  return(ticker + " wurde analysiert")
+
+
+def run_script():
+  """ Hauptfunktion um Tradingbot zu starten
+  """
+   
+  while True:
+    
+    check_stock('AAPL')
+    check_stock('TSLA')
+    check_stock('MSFT')
+    check_stock('NVDA')
+    check_stock('META')
+    check_stock('GOOGL')
+
+    # Abfrage wieder in 24 Stunden, sobald ein neuer Börsentag ist und sich DMA Datenlage aktualisiert 
+    time.sleep(24 * 60 * 60)
+
+
+
+
+
+### Starter ###
+run_script()
+
+
+
+
+
+
+
+
+
+
+
